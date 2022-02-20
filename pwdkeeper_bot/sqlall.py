26eb70a465f6e3c996794386c32413ddea6b3dbb
trah <56786791+trah01@users.noreply.github.com>
2022-02-20 20:29:20 +0800

    调整数据库连接，修正长时间不用数据库无法连接的问题

--- pwdkeeper_bot/sqlall.py
+++ pwdkeeper_bot/sqlall.py
@@ -9,22 +9,28 @@ class sqlf:
     f = open(yamlPath, 'r', encoding='utf-8')  # open方法打开直接读出来
     cfg = f.read()
 
-    global word, tableu #自定义的关键字和表
+    global word, tableu,user_ini #自定义的关键字和表
 
     user_ini = yaml.load(cfg)
     word = user_ini["word"]
     tableu = user_ini["table"]
 
-    db = pymysql.connect(host=user_ini["host"],     #自定义的数据库连接
-                         port=int(user_ini["port"]),
-                         user=user_ini["user"],
-                         password=user_ini["password"],
-                         database=user_ini["database"]
-                         )
-   
-    cursor = db.cursor()     # 使用 cursor() 方法创建一个游标对象 cursor
+    # db = pymysql.connect(host=user_ini["host"],     #自定义的数据库连接
+    #                      port=int(user_ini["port"]),
+    #                      user=user_ini["user"],
+    #                      password=user_ini["password"],
+    #                      database=user_ini["database"]
+    #                      )
+    # cursor = db.cursor()     # 使用 cursor() 方法创建一个游标对象 cursor
 
     def query(list):
+        db = pymysql.connect(host=user_ini["host"],     #自定义的数据库连接
+                            port=int(user_ini["port"]),
+                            user=user_ini["user"],
+                            password=user_ini["password"],
+                            database=user_ini["database"]
+                            )
+        cursor = db.cursor()     # 使用 cursor() 方法创建一个游标对象 cursor
         b = []
         if len(list) == 1:
             if list[0] in (word):
@@ -39,9 +45,11 @@ class sqlf:
             else:
                 sql = f"SELECT {word[0]},{word[1]},{word[2]} FROM {tableu} WHERE  {word[0]} ='{str(list[0])}' AND {word[1]} ='{str(list[1])}' "     #软件名用户名的结合查询
         try:
-            sqlf.cursor.execute(sql)
-            results = sqlf.cursor.fetchall()
-            sqlf.db.commit()
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            db.commit()
+            db.close()
+            cursor.close()
             if results:
                 for row in results:
                     a = []
@@ -54,8 +62,15 @@ class sqlf:
                 return b, "没有查到"
         except:
             return b, "失败"
-
+        
     def insert(list):
+        db = pymysql.connect(host=user_ini["host"],     #自定义的数据库连接
+                            port=int(user_ini["port"]),
+                            user=user_ini["user"],
+                            password=user_ini["password"],
+                            database=user_ini["database"]
+                            )
+        cursor = db.cursor()     # 使用 cursor() 方法创建一个游标对象 cursor
         sql = f"insert into {tableu} ({word[0]},{word[1]},{word[2]}) values ('{str(list[0])}','{str(list[1])}','{str(list[2])}')"
         list_a = []
         list_a.append(str(list[0]))
@@ -63,8 +78,10 @@ class sqlf:
         _, yz = sqlf.query(list_a)
         if str(yz) != "yes":    #验证是否存在，仅针对软件名和用户名进行重复验证
             try:
-                sqlf.cursor.execute(sql)
-                sqlf.db.commit()
+                cursor.execute(sql)
+                db.commit()
+                db.close()
+                cursor.close()
                 return "插入成功"
             except:
                 return "插入失败"
@@ -72,6 +89,13 @@ class sqlf:
             return "已经存在"
 
     def modify(list):
+        db = pymysql.connect(host=user_ini["host"],     #自定义的数据库连接
+                            port=int(user_ini["port"]),
+                            user=user_ini["user"],
+                            password=user_ini["password"],
+                            database=user_ini["database"]
+                            )
+        cursor = db.cursor()     # 使用 cursor() 方法创建一个游标对象 cursor
         sql = f"update {tableu} set {word[2]} ='{str(list[2])}' WHERE  {word[0]} ='{str(list[0])}' and  {word[1]} ='{str(list[1])}' "
         list_a = []
         list_a.append(str(list[0]))
@@ -79,8 +103,10 @@ class sqlf:
         _, yz = sqlf.query(list_a)
         if str(yz) == "yes":        #是否存在该数据查询
             try:
-                sqlf.cursor.execute(sql)
-                sqlf.db.commit()
+                cursor.execute(sql)
+                db.commit()
+                db.close()
+                cursor.close()
                 return "修改成功"
             except:
                 return "修改失败"
@@ -88,11 +114,20 @@ class sqlf:
             return "没有这个东西"
 
     def delete(list):
+        db = pymysql.connect(host=user_ini["host"],     #自定义的数据库连接
+                            port=int(user_ini["port"]),
+                            user=user_ini["user"],
+                            password=user_ini["password"],
+                            database=user_ini["database"]
+                            )
+        cursor = db.cursor()     # 使用 cursor() 方法创建一个游标对象 cursor
         list_a = []
         if str(list[0]) in (word):
             sql = f"delete from {tableu} WHERE {str(list[0])} ='{str(list[1])}'"        #单数据类型删除，用户名等
         elif str(list[0]) == "allthing":                #使用allthing，稍微复杂一点，避免误操
-            sql = sqlf.cursor.execute(f"delete from {tableu}")
+            sql = cursor.execute(f"delete from {tableu}")
+            db.close()
+            cursor.close()
             return "数据已经清空"
         else:
             sql = f"delete from {tableu} WHERE {word[0]} ='{str(list[0])}' and  {word[1]} ='{str(list[1])}' "       #软件名用户名组合匹配删除
@@ -101,8 +136,10 @@ class sqlf:
         _, yz = sqlf.query(list_a)
         if str(yz) == "yes":
             try:
-                sqlf.cursor.execute(sql)
-                sqlf.db.commit()
+                cursor.execute(sql)
+                db.commit()
+                db.close()
+                cursor.close()
                 return "删除成功"
             except:
                 return "删除失败"
